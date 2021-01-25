357febc221a13e2e738b74076973e2609e58f905
KangDroid <hyunwoo286@gmail.com>
2021-01-25 16:29:48 +0900

    Fix DB created twice

--- src/main/kotlin/com/navi/server/MainServer.kt
+++ src/main/kotlin/com/navi/server/MainServer.kt
@@ -1,12 +1,9 @@
 package com.navi.server
 
-import com.navi.server.component.FileConfigurationComponent
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.context.properties.EnableConfigurationProperties
 
 @SpringBootApplication
-@EnableConfigurationProperties(FileConfigurationComponent::class)
 class MainServer
 
 fun main(args: Array<String>) {
