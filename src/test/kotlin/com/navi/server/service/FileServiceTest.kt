09058102c1d93d24a2b19ce267758698ac5c7ff7
0JIEUN0 <jieun4436@naver.com>
2021-02-08 22:36:09 +0900

    test fileUpload Service

--- src/test/kotlin/com/navi/server/service/FileServiceTest.kt
+++ src/test/kotlin/com/navi/server/service/FileServiceTest.kt
@@ -1,25 +1,53 @@
 package com.navi.server.service
 
+import com.navi.server.component.FileConfigurationComponent
 import com.navi.server.domain.FileEntity
 import com.navi.server.domain.FileRepository
 import com.navi.server.dto.FileResponseDTO
 import com.navi.server.dto.FileSaveRequestDTO
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.After
+import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.mock.web.MockMultipartFile
 import org.springframework.test.context.junit4.SpringRunner
+import java.io.File
+import java.nio.file.Path
+import java.nio.file.Paths
+import javax.annotation.PostConstruct
 
 @RunWith(SpringRunner::class)
 @SpringBootTest
 class FileServiceTest {
+
     @Autowired
     private lateinit var fileRepository: FileRepository
 
     @Autowired
     private lateinit var fileService: FileService
+
+    @Autowired
+    private lateinit var fileConfigurationComponent: FileConfigurationComponent
+
+    private lateinit var trashRootObject: File
+
+    @Before
+    fun initEnvironment() {
+        // Create trash directory
+        trashRootObject = File(fileConfigurationComponent.serverRoot)
+        trashRootObject.mkdir()
+    }
+
+    @After
+    fun destroyEnvironment() {
+        if (trashRootObject.exists()) {
+            trashRootObject.deleteRecursively()
+        }
+        fileRepository.deleteAll()
+    }
 
     // Test variable
     private val fileNameTest: String = "TESTING_FILENAME"
@@ -31,10 +59,6 @@ class FileServiceTest {
     private val fileCreatedDateTest: String = "TEST_CREATED_DATE"
     private val fileSizeTest: String = "500Byte"
 
-    @After
-    fun clearDB() {
-        fileRepository.deleteAll()
-    }
 
     @Test
     fun isSavingWorks() {
@@ -108,5 +132,33 @@ class FileServiceTest {
             assertThat(fileCreatedDate).isEqualTo(fileCreatedDateTest)
             assertThat(fileSize).isEqualTo(fileSizeTest)
         }
+    }
+
+    @Test
+    fun fileUploadTest(){
+        /*
+        fileConfigurationComponent.populateInitialDB()
+
+        // make uploadFile
+        val uploadFileName = "uploadTest.txt"
+        val file = File(uploadFileName)
+        var content = "file upload test file!".toByteArray()
+        val multipartFile = MockMultipartFile(uploadFileName, uploadFileName, "text/plain", content)
+
+        // file upload
+        fileService.fileUpload(multipartFile)
+
+        // Assert
+        val targetFile = File(trashRootObject.absolutePath, uploadFileName)
+
+        val resultFromDB = fileRepository.findAll().find { it.fileName == targetFile.absolutePath }
+        resultFromDB?.let { assertThat(resultFromDB.fileName).isEqualTo(targetFile.absolutePath) } ?: throw Exception("ERROR:: no $uploadFileName")
+
+        val resultFromServer = trashRootObject.listFiles().find { it.isFile && it.absolutePath == targetFile.absolutePath }
+        resultFromServer?.let { assertThat(resultFromServer.absolutePath).isEqualTo(targetFile.absolutePath) } ?: throw Exception("ERROR:: no ${targetFile.absolutePath}")
+
+
+
+         */
     }
 }
