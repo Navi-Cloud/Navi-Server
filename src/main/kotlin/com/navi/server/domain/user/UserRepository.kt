700dacb2da862ca27b69c222bf69955ebc87db7a
KangDroid <hyunwoo286@gmail.com>
2021-03-21 01:10:24 +0900

    UserRepository: Migrate findAllByOrderByIdDesc to findAllFileObject

--- src/main/kotlin/com/navi/server/domain/user/UserRepository.kt
+++ src/main/kotlin/com/navi/server/domain/user/UserRepository.kt
@@ -1,8 +1,19 @@
 package com.navi.server.domain.user
 
+import com.navi.server.domain.FileObject
+import com.navi.server.error.exception.NotFoundException
 import org.bson.types.ObjectId
 import org.springframework.data.mongodb.repository.MongoRepository
 
 interface UserRepository: MongoRepository<User, ObjectId> {
     fun findByUserName(inputUserName: String): User?
+
+    // Custom function
+    fun findAllFileObject(inputUserName: String): List<FileObject> {
+        val user: User = findByUserName(inputUserName) ?: run {
+            throw NotFoundException("Cannot find username with: ${inputUserName}.")
+        }
+
+        return user.fileList
+    }
 }
