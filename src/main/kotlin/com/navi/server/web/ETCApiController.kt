d4d32655ad2293002a8fa1173bba6c67b2c563f8
KangDroid <hyunwoo286@gmail.com>
2021-07-09 12:50:45 +0900

    Introduce Server Reset command for testing

--- src/main/kotlin/com/navi/server/web/ETCApiController.kt
+++ src/main/kotlin/com/navi/server/web/ETCApiController.kt
@@ -1,15 +1,30 @@
 package com.navi.server.web
 
+import com.navi.server.domain.user.User
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.core.env.Environment
+import org.springframework.data.mongodb.core.MongoTemplate
+import org.springframework.data.mongodb.core.query.Query
+import org.springframework.data.mongodb.core.remove
+import org.springframework.data.mongodb.gridfs.GridFsTemplate
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-class ETCApiController {
+class ETCApiController(
+    private val gridFsTemplate: GridFsTemplate,
+    private val mongoTemplate: MongoTemplate
+) {
     @Autowired
     private lateinit var environment: Environment
+
+    @GetMapping("/api/remove")
+    fun removeAllData(): String {
+        gridFsTemplate.delete(Query())
+        mongoTemplate.remove<User>(Query())
+        return "OK"
+    }
 
     @GetMapping("/profile")
     fun getCurrentProfile(): ResponseEntity<String> {
